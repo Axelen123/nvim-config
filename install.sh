3d852dc7b5022d2c8853036e40ecfa2d63d0f929
Axelen123 <axelakesson05@hotmail.com>
2019-06-23 19:47:23 +0200

    Add svelte-language-server to install script

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh >
 sh ./installer.sh ~/.config/nvim/dein
 rm installer.sh
 rustup component add rls rust-analysis rust-src
-npm i -g neovim
+npm i -g neovim svelte-language-server
 pip2 install pynvim
 pip3 install pynvim
 gem install neovim
